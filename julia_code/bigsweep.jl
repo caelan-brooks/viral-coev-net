22fdce69d0045f88d55cbfa31b8661d401b86360
dancewartz <dswartz@mit.edu>
2023-09-13 14:46:32 -0400

    fix

--- julia_code/bigsweep.jl
+++ julia_code/bigsweep.jl
@@ -32,7 +32,13 @@ const immune_density = zeros(Float64, length(x))
 function run_single_simulation(args)
     migration_rate, simulation_number = args
 
-    # ... (rest of your function definition)
+    population1 = Population(L, dx, r, M, beta, alpha, gamma, D, Nh, viral_density, immune_density)
+    population2 = Population(L, dx, r, M, beta, alpha, gamma, D, Nh, viral_density2, immune_density)
+    
+    migration_matrix = [0 migration_rate; migration_rate 0]
+
+    network = Network([population1, population2], migration_matrix)
+    simulation = Simulation(network, dt, duration)
 
     try
         @time run_simulation!(simulation)
@@ -43,12 +49,13 @@ function run_single_simulation(args)
     catch e
         println("Error in simulation with args $args: $e")
     end
+    
 end
 
 function main()
     migration_rates = exp10.(LinRange(-6, 0.5, 9)) # Example migration rates to sweep over
     start_rep = 0
-    num_replicates = 100
+    num_replicates = 12000
 
     # Creating a list of tuples with migration rates and simulation numbers
     simulation_args = [(rate, num) for rate in migration_rates for num in start_rep:(start_rep + num_replicates - 1)]
