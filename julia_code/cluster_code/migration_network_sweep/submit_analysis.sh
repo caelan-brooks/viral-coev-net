7724dd49de374ddb1efd48d6897eba656d8adc99
dancewartz <dswartz@mit.edu>
2023-11-01 17:10:26 -0400

    making the analysis much faster hopefully

--- julia_code/cluster_code/migration_network_sweep/submit_analysis.sh
+++ julia_code/cluster_code/migration_network_sweep/submit_analysis.sh
@@ -2,7 +2,7 @@
 #SBATCH -N 1                   # Number of nodes
 #SBATCH -n 8                   # Number of cores
 #SBATCH -p sched_mit_hill      # Partition name
-#SBATCH --mem-per-cpu=4000     # Memory per CPU (adjust this based on your requirements)
+#SBATCH --mem-per-cpu=2000     # Memory per CPU (adjust this based on your requirements)
 #SBATCH -o analysis_output.log # Output file name
 #SBATCH -e analysis_error.log  # Error file name
 
