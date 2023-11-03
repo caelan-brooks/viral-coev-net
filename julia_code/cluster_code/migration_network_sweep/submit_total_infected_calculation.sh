8046f25bb8955f277053c4b669ffbe4bd18296d8
dswartz <dswartz@eofe7.mit.edu>
2023-11-02 21:48:02 -0400

    i think i got stuff to delete

--- julia_code/cluster_code/migration_network_sweep/submit_total_infected_calculation.sh
+++ julia_code/cluster_code/migration_network_sweep/submit_total_infected_calculation.sh
@@ -2,7 +2,7 @@
 #SBATCH -N 1                # Number of nodes
 #SBATCH -n 8               # Number of cores
 #SBATCH -p sched_mit_hill   # Partition name
-#SBATCH --mem-per-cpu=4000  # Memory per CPU (adjust this based on your requirements)
+#SBATCH --mem-per-cpu=800  # Memory per CPU (adjust this based on your requirements)
 #SBATCH --array=1-50        # 5 network sizes times 10 migration rates
 
 # Path to the Julia project/environment
@@ -11,4 +11,4 @@ JULIA_PROJECT_PATH="/home/dswartz/viral-coev-net/julia_code/cluster_code/cluster
 module load julia
 
 # Execute your Julia script using the specific project and all available cores
-julia --project=$JULIA_PROJECT_PATH -t auto analyze_infected_trajectories.jl
+julia --project=$JULIA_PROJECT_PATH -t auto preprocess_total_infected.jl
