ba884480d8e9335687ef8cf3a0951833e74def06
dancewartz <dswartz@mit.edu>
2023-11-15 09:27:34 -0500

    plotting and videos! plus variance analysis

--- julia_code/variance_analysis.jl
+++ julia_code/variance_analysis.jl
@@ -14,11 +14,12 @@ const OUTPUT_PATH = "plotted_results_new/"
 
 const MIGRATION_RATES = exp10.(LinRange(-7.0, -0.5, 9)) # Example migration rates to sweep over
 
-function plot_variance_vs_infected(migration_rate, idx)
+function plot_variance_vs_infected(migration_rate)
     files = glob("simulation_results_migration_$(migration_rate)_replicate_*.jld2", DIRECTORY_PATH)
     println(length(files))
+    short_migration_rate = @sprintf("%.2e", migration_rate)  # Format migration rate
 
-    p = plot(;ylim=(0,1), xscale=:log10, xlabel="Total Infected", ylabel="Variance", title="Variance vs Total Infected for Migration Rate $migration_rate",legend=:none)
+    p = plot(;ylim=(0,1), xscale=:log10, xlabel="Total Infected", ylabel="Variance", title="MR: $short_migration_rate", legend=:none)
 
     # Iterate over files
     for file in files
@@ -46,17 +47,26 @@ function plot_variance_vs_infected(migration_rate, idx)
                 filtered_variances = variances_per_deme[deme, reg]
 
                 # Plotting with see-through markers
-                scatter!(p, filtered_infected, filtered_variances, m=(:cross, 4, 0.5), color=(deme == 1 ? :blue : :red))
+                scatter!(p, filtered_infected, filtered_variances, m=(:cross, 2, 0.5), color=(deme == 1 ? :blue : :red))
             end
         end
     end
 
     # Save the plot
-    savefig(p, "$(OUTPUT_PATH)/variance_vs_infected_mr$(idx).png")
+    # savefig(p, "$(OUTPUT_PATH)/variance_vs_infected_mr$(idx).png")
+    return p
 end
 
-# Assuming MIGRATION_RATES and plot_variance_vs_infected function are defined
+# Main loop to create a 3x3 grid of plots
+all_plots = []
 for (idx, migration_rate) in enumerate(MIGRATION_RATES)
     println("Processing migration rate: $migration_rate")
-    plot_variance_vs_infected(migration_rate, idx)
+    p = plot_variance_vs_infected(migration_rate)
+    push!(all_plots, p)
 end
+
+# Combine all plots into a 3x3 grid layout
+combined_plot = plot(all_plots..., layout=(3, 3), size=(900, 900))
+
+# Save the combined plot
+savefig(combined_plot, "$(OUTPUT_PATH)/combined_variance_vs_infected.png")
