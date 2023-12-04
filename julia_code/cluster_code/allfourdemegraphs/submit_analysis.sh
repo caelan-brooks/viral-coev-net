0ba005afa0f639b6ad559da006cccfcfbbcd99f0
dancewartz <72061205+dancewartz@users.noreply.github.com>
2023-12-04 15:15:06 -0500

    space

--- julia_code/cluster_code/allfourdemegraphs/submit_analysis.sh
+++ julia_code/cluster_code/allfourdemegraphs/submit_analysis.sh
@@ -2,7 +2,7 @@
 #SBATCH -N 1                # Number of nodes
 #SBATCH -n 8               # Number of cores
 #SBATCH -p sched_mit_hill   # Partition name
-#SBATCH --mem-per-cpu= 1000  # Memory per CPU (adjust this based on your requirements)
+#SBATCH --mem-per-cpu=1000  # Memory per CPU (adjust this based on your requirements)
 
 # Path to the Julia project/environment
 JULIA_PROJECT_PATH="/home/dswartz/viral-coev-net/julia_code/cluster_code/cluster_project"
