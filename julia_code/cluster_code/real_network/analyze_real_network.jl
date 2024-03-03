fe30f63b7b0aac623ddd17c2237f384a4db30e4d
dancewartz <72061205+dancewartz@users.noreply.github.com>
2024-03-03 08:53:06 -0500

    bugfix

--- julia_code/cluster_code/real_network/analyze_real_network.jl
+++ julia_code/cluster_code/real_network/analyze_real_network.jl
@@ -15,83 +15,8 @@ function read_data(file)
     open(deserialize, file)
 end
 
-function process_and_save_histograms(outbreak_deme_idx; cutoff = 100)
-    output_subdirectory = joinpath(OUTPUT_DIRECTORY, "outbreak_deme_idx_$outbreak_deme_idx")
-    replicate_files = glob("*.jld2", output_subdirectory)
-
-    antigenic_variance_deme1 = []
-    antigenic_variance_deme2 = []
-    peak_time_difference = []
-    variance_difference = []
-
-    for file in replicate_files
-        total_infected_per_deme, antigenic_variance_per_deme = read_data(file)
-        
-        # Find indices of maximal infection for each deme because dt = 1/20 and thin by = 20 index corrresponds to "real" time!
-        max_infected_idx_deme1 = argmax(total_infected_per_deme[1, :])
-        min_infected_idx_deme1 = argmin(total_infected_per_deme[1, :])
-        
-        max_infected_idx_deme2 = argmax(total_infected_per_deme[2, :])
-        
-        
-        # Check if maximal infection is greater than cutoff before recording the antigenic variance
-        if total_infected_per_deme[1, max_infected_idx_deme1] > cutoff
-            push!(antigenic_variance_deme1, antigenic_variance_per_deme[1, max_infected_idx_deme1])
-        end
-        
-        # if total_infected_per_deme[2, max_infected_idx_deme2] > cutoff
-        #     push!(antigenic_variance_deme2, antigenic_variance_per_deme[2, max_infected_idx_deme2])
-        #     push!(peak_time_difference, max_infected_idx_deme2 - max_infected_idx_deme1)
-        #     push!(variance_difference, antigenic_variance_per_deme[2, max_infected_idx_deme2] - antigenic_variance_per_deme[1, max_infected_idx_deme1])
-        # end
-        
-        first_cross_idx_deme2 = findfirst(total_infected_per_deme[2, :] .>= cutoff)
-        
-        if !isnothing(first_cross_idx_deme2)
-            if first_cross_idx_deme2 > min_infected_idx_deme1
-                println("LATE")
-                flush(stdout)
-            end
-     
-            if total_infected_per_deme[2, max_infected_idx_deme2] > cutoff && first_cross_idx_deme2 <= min_infected_idx_deme1
-                push!(antigenic_variance_deme2, antigenic_variance_per_deme[2, max_infected_idx_deme2])
-                push!(peak_time_difference, max_infected_idx_deme2 - max_infected_idx_deme1)
-                push!(variance_difference, antigenic_variance_per_deme[2, max_infected_idx_deme2] - antigenic_variance_per_deme[1, max_infected_idx_deme1])
-            end
-        end
-    end
-
-    # Save the antigenic variance data
-    CSV.write(joinpath(CSV_OUTPUT_DIRECTORY, "antigenic_variance_deme1_migration_rate_idx_$migration_rate_idx.csv"), DataFrame(AntigenicVariance=antigenic_variance_deme1))
-    CSV.write(joinpath(CSV_OUTPUT_DIRECTORY, "antigenic_variance_deme2_migration_rate_idx_$migration_rate_idx.csv"), DataFrame(AntigenicVariance=antigenic_variance_deme2))
-    CSV.write(joinpath(CSV_OUTPUT_DIRECTORY, "peak_time_difference_migration_rate_idx_$migration_rate_idx.csv"), DataFrame(PeakTimeDifference=peak_time_difference))
-    CSV.write(joinpath(CSV_OUTPUT_DIRECTORY, "variance_difference_migration_rate_idx_$migration_rate_idx.csv"), DataFrame(VarianceDifference=variance_difference))
-end
-
-function process_trajectories(migration_rate_idx, num_replicates)
-    output_subdirectory = joinpath(OUTPUT_DIRECTORY, "migration_rate_idx_$migration_rate_idx")
-    replicate_files = glob("*.jld2", output_subdirectory)
-    
-    # Initialize a matrix to store all trajectories
-    trajectories_matrix = []
-
-    for i in 1:min(num_replicates, length(replicate_files))
-        file = replicate_files[i]
-        total_infected_per_deme, _ = read_data(file)
-        total_infected = sum(total_infected_per_deme, dims=1)  # Sum over the first index
-        push!(trajectories_matrix, total_infected[:]')
-    end
-
-    # Convert the array of trajectories into a DataFrame
-    traj_df = DataFrame(trajectories_matrix)
-
-    # Save to CSV
-    CSV.write(joinpath(CSV_OUTPUT_DIRECTORY, "trajectories_migration_rate_idx_$migration_rate_idx.csv"), traj_df)
-end
-
-
 # Initialize survival_probabilities as an array with NaN
-survival_probabilities = fill(NaN, length(MIGRATION_RATES))
+survival_probabilities = fill(NaN, length(OUTBREAK_DEMES))
 
 # Loop over each migration rate
 for (idx, outbreak_deme) in enumerate(OUTBREAK_DEMES)
