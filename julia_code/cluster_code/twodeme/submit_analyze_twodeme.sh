7fea69d5a97d5c94bd0f7d0145bd21e6c94e8514
dancewartz <72061205+dancewartz@users.noreply.github.com>
2024-03-07 11:09:20 -0500

    multithreading analysis

--- julia_code/cluster_code/twodeme/submit_analyze_twodeme.sh
+++ julia_code/cluster_code/twodeme/submit_analyze_twodeme.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 #SBATCH -N 1                # Number of nodes
-#SBATCH -n 1               # Number of cores
+#SBATCH -n 8               # Number of cores
 #SBATCH -p sched_mit_hill   # Partition name
 #SBATCH --mem-per-cpu=500  # Memory per CPU (adjust this based on your requirements)
+#SBATCH -t 0-01:00   
 
 # Path to the Julia project/environment
 JULIA_PROJECT_PATH="/home/dswartz/viral-coev-net/julia_code/cluster_code/cluster_project"
