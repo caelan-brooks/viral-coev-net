96b885a36d5b2fa1bcb1125eaba6e5af31cb9743
dancewartz <dswartz@mit.edu>
2024-05-04 18:42:45 -0400

    more jobs in array

--- julia_code/cluster_code/twodeme/submit_twodemesweep.sh
+++ julia_code/cluster_code/twodeme/submit_twodemesweep.sh
@@ -3,7 +3,7 @@
 #SBATCH -n 16               # Number of cores
 #SBATCH -p sched_mit_hill   # Partition name
 #SBATCH --mem-per-cpu=1000  # Memory per CPU (adjust this based on your requirements)
-#SBATCH --array=1-12        # Create a job array for 10 jobs
+#SBATCH --array=1-14        # Create a job array for 10 jobs
 
 # Path to the Julia project/environment
 JULIA_PROJECT_PATH="/home/dswartz/viral-coev-net/julia_code/cluster_code/cluster_project"
