a0cedd7e7f374431bf5589cc4bd5d5c6c8d60149
dancewartz <dswartz@mit.edu>
2024-05-05 14:37:35 -0400

    next batch

--- julia_code/cluster_code/twodeme/twodemesweep.jl
+++ julia_code/cluster_code/twodeme/twodemesweep.jl
@@ -28,7 +28,7 @@ const DURATION = 80.0
 const DT = 0.05
 const THIN_BY = 20
 const NUM_REPLICATES = 5000
-const START_REPLICATE = 1 
+const START_REPLICATE = 5001 
 
 function run_single_simulation(args)
     # Unpack arguments
