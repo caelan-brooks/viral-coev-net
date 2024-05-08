0205aa86b82e659b5515757961772b84fdaade16
dancewartz <72061205+dancewartz@users.noreply.github.com>
2024-05-08 17:25:59 -0400

    Revert "updating some of the analysis figures to see what they look like"
    This reverts commit e048b7a5e9f2b2011822b3f1dcf76be908b9409e.

--- final_plots_viral_coev/two_deme_two_figs.py
+++ final_plots_viral_coev/two_deme_two_figs.py
@@ -201,10 +201,10 @@ csv_file_b = "special_case_antigenic_variance_deme1_migration_rate_idx_1.csv"  #
 data_b = pd.read_csv(csv_file_b)
 
 # Filter and binning setup
-xmin, xmax = 0.1, 0.25  # Adjust this range as needed
-# xmin, xmax = 0.16, 0.19
+xmin, xmax = 0.15, 0.25  # Adjust this range as needed
+xmin, xmax = 0.16, 0.19
 data_b = data_b[(data_b['AntigenicVariance'] >= xmin) & (data_b['AntigenicVariance'] <= xmax)]
-num_bins = 30  # Number of bins
+num_bins = 10  # Number of bins
 hist, bin_edges = np.histogram(data_b['AntigenicVariance'], bins=num_bins, range=(xmin, xmax), density=True)
 
 # Calculate survival proportions and standard errors
