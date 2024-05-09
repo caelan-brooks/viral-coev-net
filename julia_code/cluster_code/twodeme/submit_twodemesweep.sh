93cb72e76565f2873860bb38683d048974733c66
dancewartz <dswartz@mit.edu>
2024-05-08 23:33:14 -0400

    lowering resource requests for this run

--- julia_code/cluster_code/twodeme/submit_twodemesweep.sh
+++ julia_code/cluster_code/twodeme/submit_twodemesweep.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 #SBATCH -N 1                # Number of nodes
-#SBATCH -n 12               # Number of cores
+#SBATCH -n 4               # Number of cores
 #SBATCH -p sched_mit_hill   # Partition name
 #SBATCH --mem-per-cpu=1000  # Memory per CPU (adjust this based on your requirements)
 #SBATCH --array=1-14        # Create a job array for 10 jobs
-#SBATCH -t 0-06:00
 
 # Path to the Julia project/environment
 JULIA_PROJECT_PATH="/home/dswartz/viral-coev-net/julia_code/cluster_code/cluster_project"
