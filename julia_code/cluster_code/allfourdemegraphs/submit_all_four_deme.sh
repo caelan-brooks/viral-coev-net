d8db0be4a938406cce73e6cdf5a4007a8ca9b609
dancewartz <72061205+dancewartz@users.noreply.github.com>
2024-05-15 13:15:03 -0400

    changed how julia is loaded

--- julia_code/cluster_code/allfourdemegraphs/submit_all_four_deme.sh
+++ julia_code/cluster_code/allfourdemegraphs/submit_all_four_deme.sh
@@ -8,7 +8,7 @@
 # Path to the Julia project/environment
 JULIA_PROJECT_PATH="/home/dswartz/viral-coev-net/julia_code/cluster_code/cluster_project"
 
-module load julia
+module add /home/software/modulefiles/julia/
 
 # Execute your Julia script using the specific project and all available cores
 julia --project=$JULIA_PROJECT_PATH -t auto allfourdemesweep.jl
