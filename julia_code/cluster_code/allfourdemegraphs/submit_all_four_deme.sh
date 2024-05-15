197f7d37251011b9ab4ecc47171a64f944a4decc
dancewartz <72061205+dancewartz@users.noreply.github.com>
2024-05-15 13:04:38 -0400

    SETTING UP THE 4 DEME SWEEP WITH THE PL METHOD

--- julia_code/cluster_code/allfourdemegraphs/submit_all_four_deme.sh
+++ julia_code/cluster_code/allfourdemegraphs/submit_all_four_deme.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH -N 1                # Number of nodes
-#SBATCH -n 8               # Number of cores
+#SBATCH -n 6               # Number of cores
 #SBATCH -p sched_mit_hill   # Partition name
-#SBATCH --mem-per-cpu=4000  # Memory per CPU (adjust this based on your requirements)
+#SBATCH --mem-per-cpu=2000  # Memory per CPU (adjust this based on your requirements)
 #SBATCH --array=1-110        # Create a job array for 10 jobs
 
 # Path to the Julia project/environment
