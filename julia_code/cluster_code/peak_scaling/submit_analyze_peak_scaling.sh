790dbc2f961df23171d4cca9abe4811c9780558a
dancewartz <72061205+dancewartz@users.noreply.github.com>
2024-09-10 15:58:31 -0400

    more cores for analysis

--- julia_code/cluster_code/peak_scaling/submit_analyze_peak_scaling.sh
+++ julia_code/cluster_code/peak_scaling/submit_analyze_peak_scaling.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH -N 1                # Number of nodes
-#SBATCH -n 4               # Number of cores
+#SBATCH -n 8               # Number of cores
 #SBATCH -p sched_mit_hill   # Partition name
 #SBATCH --mem-per-cpu=1000  # Memory per CPU (adjust this based on your requirements)
 #SBATCH -t 0-06:00          # time taken is 4 hrs
