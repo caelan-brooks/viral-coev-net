397fd78460e0af56e1de4da92bb7a68c6b1b6cc3
dancewartz <72061205+dancewartz@users.noreply.github.com>
2024-09-16 14:28:33 -0400

    use a local install of julia to avoid job array issues

--- julia_code/cluster_code/peak_scaling/submit_analyze_peak_scaling.sh
+++ julia_code/cluster_code/peak_scaling/submit_analyze_peak_scaling.sh
@@ -8,7 +8,9 @@
 # Path to the Julia project/environment
 JULIA_PROJECT_PATH="/home/dswartz/viral-coev-net/julia_code/cluster_code/cluster_project"
 
-module add /home/software/modulefiles/julia/
+# use my local install of julia
+export PATH="~/julia-1.9.0/bin:$PATH"
+which julia
 
 # Execute your Julia script using the specific project and all available cores
 julia --project=$JULIA_PROJECT_PATH -t auto analyze_peak_scaling.jl
