f00fff08d27b2712a49377c8d4f73eb0a73d6743
dancewartz <dswartz@mit.edu>
2025-04-11 00:07:14 -0400

    pop_size_het

---
+++ julia_code/cluster_code/pop_size_het/submit_real_network.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH -N 1                # Number of nodes
+#SBATCH -n 6               # Number of cores
+#SBATCH -p sched_mit_hill   # Partition name
+#SBATCH -t 0-11:58          # time taken is 4 hrs
+#SBATCH --mem-per-cpu=3000  # Memory per CPU (adjust this based on your requirements)
+#SBATCH --array=1-30        # Create a job array for 10 jobs
+
+# Path to the Julia project/environment
+JULIA_PROJECT_PATH="/home/dswartz/viral-coev-net/julia_code/cluster_code/cluster_project"
+
+# use my local install of julia
+export PATH="~/julia-1.9.0/bin:$PATH"
+# module add /home/software/modulefiles/julia/
+which julia
+
+# Execute your Julia script using the specific project and all available cores
+julia --project=$JULIA_PROJECT_PATH -t 12 real_network_sweep.jl
